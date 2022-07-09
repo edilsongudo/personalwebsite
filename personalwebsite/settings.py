0a37eebc83b446bbe204cd58bdded7eca16b6402
Edilson <edilsongudo@gmail.com>
2022-07-10 00:30:52 +0200

    Dinamic Debug Value

--- personalwebsite/settings.py
+++ personalwebsite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get('DEBUG') else False
 
 ALLOWED_HOSTS = [
     'edilsongudo.herokuapp.com',
